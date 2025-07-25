58abbbc7f01d7f2d74bb2977632bd9e9a3bd8064
lukewasham11 <118244573+lwasham1@users.noreply.github.com>
2025-07-25 01:50:52 -0500

    Add files via upload

---
+++ LobbyCreation.jsx
@@ -0,0 +1,13 @@
+import { Link, Outlet } from "react-router-dom";
+import { useState } from "react";
+function LobbyCreation(){
+
+
+
+
+    return(
+        <h2>your momma</h2>
+    )
+
+}
+export default LobbyCreation
